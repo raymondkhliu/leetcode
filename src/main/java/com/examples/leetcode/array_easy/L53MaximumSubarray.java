167eae18be5c43a464bcbeabe16ba86ac641b924
Raymond Liu <raymond.kh.liu@gmail.com>
2021-04-05 21:57:32 -0700

    finished leetcode L52

--- src/main/java/com/examples/leetcode/array_easy/L53MaximumSubarray.java
+++ src/main/java/com/examples/leetcode/array_easy/L53MaximumSubarray.java
@@ -2,15 +2,13 @@ package com.examples.leetcode.array_easy;
 
 /** https://leetcode.com/problems/maximum-subarray/ */
 public class L53MaximumSubarray {
-
     public int maxSubArray(int[] nums) {
         int max = Integer.MIN_VALUE;
-        int sum = 0;
-        for (int i : nums) {
-            sum += i;
-            max = Math.max(max, sum);
-            if (sum < 0) {
-                sum = 0;
+        int count = 0;
+        for (int num : nums) {
+            max = Math.max(count += num, max);
+            if (count < 0) {
+                count = 0;
             }
         }
         return max;
